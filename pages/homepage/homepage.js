8bb2f80ba770f3b87cca580934e2da05012d0911
Themba Dombo <themba@bbd.co.za>
2021-03-17 23:25:42 +0200

    burger component to render with sub components

---
+++ pages/homepage/homepage.js
@@ -0,0 +1,20 @@
+Page({
+  data: {
+    burger:[{ img: '/assets/pictures/spongebob_PNG8.png',
+    title: 'Spongebob',
+    calories: '230KCal',
+    ingredients: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium'
+  },
+{ img: '/assets/pictures/spongebob_PNG8.png',
+    title: 'Spongebob',
+    calories: '230KCal',
+    ingredients: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium'
+  },
+{ img: '/assets/pictures/spongebob_PNG8.png',
+    title: 'Spongebob',
+    calories: '230KCal',
+    ingredients: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium'
+  }]
+   },
+  onLoad() {},
+});
