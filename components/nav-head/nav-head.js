c093a80fbe99d654e6e0c80c2ae5e5811f81a0af
Themba Dombo <themba@bbd.co.za>
2021-03-19 15:45:55 +0200

    added nav-bar select css

--- components/nav-head/nav-head.js
+++ components/nav-head/nav-head.js
@@ -1,6 +1,10 @@
 Component({
   mixins: [],
-  data: {},
+  data: {
+    burgerHighLight: "highLight",
+    mealHighLight: "none",
+    sideHighLight: "none"
+    },
   props: {
     onPageChangeView: () => {}
   },
@@ -11,6 +15,23 @@ Component({
     changeView(event) {
       let info = event.target.id;
       this.props.onPageChangeView(info);
+    },
+    isHighLight(event){
+      let select = event.target.id
+      switch (select) {
+        case "burgers":
+          this.setData({burgerHighLight: "highLight", mealHighLight: "none", sideHighLight: "none"})
+          break;
+        case "meals":
+          this.setData({burgerHighLight: "none", mealHighLight: "highLight", sideHighLight: "none"})
+          break;
+        case "sides":
+          this.setData({burgerHighLight: "none", mealHighLight: "none", sideHighLight: "highLight"})
+          break;
+      
+        default:
+          break;
+      }
     }
   },
 });
